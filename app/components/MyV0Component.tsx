76b9d1157616ff1de1a29695bb2120a8a068cf60
kandy-209 <70727213+kandy-209@users.noreply.github.com>
2024-06-05 15:27:00 -0700

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,27 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+// For an initial starting point, insert your GitHub username between the double quotes here.
+// Letter casing doesn't matter.
+const yourGithubUsername = "kandy-209";
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
+// During the workshop, leave the code below as-is.
+// But when you're ready, don't be afraid to jump in, make edits, try new things, and make it your own!
+import {EmptyState} from "./components/empty-state";
+import {NotFound} from "./components/not-found";
+import {Profile} from "./components/profile";
+import {getGithubProfile} from "./lib/get-github-profile";
 
-function TestComponent() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
-        >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
+const githubToken = process.env.GITHUB_TOKEN ? process.env.GITHUB_TOKEN : false;
 
-        <V0Logo />
-      </div>
-    </div>
-  );
+export default async function Home(url: any) {
+  const profileData = await getGithubProfile(yourGithubUsername);
+  const geo = url.searchParams;
+
+  if (!profileData) {
+    return <EmptyState />;
+  }
+
+  if (profileData === "not-found") {
+    return <NotFound username={yourGithubUsername} />;
+  }
+
+  return <Profile username={yourGithubUsername} geo={geo} />;
 }
-
-export default TestComponent;
